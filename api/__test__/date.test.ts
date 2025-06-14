4ec5dedb6db9eeb2b414bdd9a32e18abcf94a1f8
Hemit Patel <hemitvpatel@gmail.com>
2025-06-13 22:32:16 -0400

    Update date.test.ts

--- api/__test__/date.test.ts
+++ api/__test__/date.test.ts
@@ -1,1 +1,9 @@
+import { convertDateFormat } from './dateConverter';
 
+describe('convertDateFormat', () => {
+  it('converts MM/DD/YYYY to YYYY-MM-DD', () => {
+    expect(convertDateFormat('01/15/2023')).toBe('2023-01-15');
+    expect(convertDateFormat('6/7/2020')).toBe('2020-06-07');
+    expect(convertDateFormat('12/5/1999')).toBe('1999-12-05');
+  });
+});
