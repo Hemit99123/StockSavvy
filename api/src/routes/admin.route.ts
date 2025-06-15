8c6c40531912523ea18c7325dbb23a4116c0d4ab
Hemit Patel <hemitvpatel@gmail.com>
2025-06-14 22:32:01 -0400

    another thing needing fix

--- api/src/routes/admin.route.ts
+++ api/src/routes/admin.route.ts
@@ -1,8 +1,8 @@
-import express from "express"
+import { Router } from "express"
 import { authenticateAdminSession } from "../middleware/session.middleware.ts";
 import adminController from "../controllers/admin.controller.ts";
 
-const router = express.Router();
+const router: Router = Router();
 
 router.post("/create-question", authenticateAdminSession, adminController.createQuestion)
 router.delete("/delete-question", authenticateAdminSession, adminController.deleteQuestion)
